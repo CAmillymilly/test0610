ab893bdb832445c77234073d58dc275fd4d69d0d
CAmillymilly <130669887+CAmillymilly@users.noreply.github.com>
2023-10-06 11:58:54 -0300

    commit

---
+++ index6.php
@@ -0,0 +1,15 @@
+<?php
+
+//PHP programa que compara as datas
+
+//Declare duas datas e inicealize-as
+$date1 = "01-01-2023";
+$date2 = "31-12-2023";
+
+//Use o operador de comparação para comparar datas
+if ($date1 > $date2)
+    echo "$date1 é mais recente que $date2";
+else    
+    echo "$date1 É mais antigo que $date2";
+
+?>
