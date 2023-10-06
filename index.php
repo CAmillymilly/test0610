97c68f5f7e3fce233bced1f55c88a4dd7d8af904
CAmillymilly <130669887+CAmillymilly@users.noreply.github.com>
2023-10-06 09:50:44 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$text = "O perigo de verdade não é que o computadores passem a pensar como humanos, mas sim que humanos passem a pensam como computadores";
+
+$result = substr($text, 0, 20); //posição incial = 0, comprimento = 20
+
+echo $result;
+
+?>
---
+++ index3.php
@@ -0,0 +1,8 @@
+<?php
+
+$text = "O perigo de verdade não é que computadores passem a pensar como humanos, mas sim que humanos passem a pensar como computadores";
+
+$result = strstr($text, "não é que"); //false é default - podemos omitir
+
+echo $result;
+?>
